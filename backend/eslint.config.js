8a4cda80325390b5a206f7377887869b4847b682
lhorean <gaudy.montero.briones@est.una.ac.cr>
2026-03-28 03:18:15 -0600

    chore: setup ESLint and Prettier with auto-format on save

---
+++ backend/eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
