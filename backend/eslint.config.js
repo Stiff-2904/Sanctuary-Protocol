4da22f7ab899d29e6f152c8f952c841cbb75c4cb
lhorean <gaudy.montero.briones@est.una.ac.cr>
2026-03-28 04:01:33 -0600

    chore: configure ESLint and Prettier for Node environment

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,17 +1,17 @@
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
-  prettier,
   {
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node,
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
     },
   },
 ];
