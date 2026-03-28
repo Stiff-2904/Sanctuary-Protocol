4ffdc59b7348c96635be5defce24ef7cbb5c5102
lhorean <gaudy.montero.briones@est.una.ac.cr>
2026-03-28 12:39:49 -0600

    feat: complete camps CRUD; use status field for logical deletion to preserve referential integrity

--- backend/src/controllers/camp.controller.js
+++ backend/src/controllers/camp.controller.js
@@ -1,4 +1,6 @@
 import { getCamps } from '../models/camp.model.js';
+import { createCamp } from '../models/camp.model.js';
+import { updateCamp } from '../models/camp.model.js';
 
 export const getCampsController = async (req, res) => {
   try {
@@ -7,6 +9,37 @@ export const getCampsController = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: 'Error to get camps',
+      error: error.message,
+    });
+  }
+};
+
+export const createCampController = async (req, res) => {
+  try {
+    const { name, location, status } = req.body;
+
+    const newCamp = await createCamp({ name, location, status });
+
+    res.status(201).json(newCamp);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error creating camp',
+      error: error.message,
+    });
+  }
+};
+
+export const updateCampController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, location, status } = req.body;
+
+    const updatedCamp = await updateCamp(id, { name, location, status });
+
+    res.json(updatedCamp);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error updating camp',
       error: error.message,
     });
   }
